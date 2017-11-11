cd910309c51d541303efb3b62db296e2c22e2bbc
Arjun Vegda <arjunvegda@users.noreply.github.com>
2017-11-11 14:44:38 -0500

    Added notes

---
+++ Chapter 1/chapter1.md
@@ -0,0 +1,7 @@
+## Chapter 1
+
+* The process of modeling the value of something with
+a set of fixed weights is called linear regression.
+It's one of the simplest machine learning algorithms
+but with machine learning the computer comes up with
+the weights by itself by looking up the training data.
--- Chapter 1/simple_value_estimator.py
+++ Chapter 1/simple_value_estimator.py
@@ -21,11 +21,4 @@ value = estimate_home_value(3800, 5)
 print("Estimated valued:")
 print(value)
 
-#Notes
-'''
- The process of modeling the value of something with 
-a set of fixed weights is called linear regression. 
-It's one of the simplest machine learning algorithms
-but with machine learning the computer comes up with
-the weights by itself by looking up the training data.
-'''
+
