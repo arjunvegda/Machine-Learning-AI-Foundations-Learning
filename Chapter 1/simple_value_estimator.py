17f74f0cc19d97d0d19e7adeaf215865ba3a84e4
Arjun Vegda <arjunvegda@users.noreply.github.com>
2017-11-11 14:20:12 -0500

    Chapter 1 completed

---
+++ Chapter 1/simple_value_estimator.py
@@ -0,0 +1,31 @@
+def estimate_home_value(size_in_sqft, number_of_bedrooms):
+
+    # Assume all homes are worth at least $50,000
+    value = 50000
+
+    # Adjust the value estimate based on the size of the house
+    value = value + (size_in_sqft * 92)
+
+    # Adjust the value estimate based on the number of bedrooms
+    value = value + (number_of_bedrooms * 10000)
+
+    return value
+
+# Estimate the value of our house:
+# - 5 bedrooms
+# - 3800 sq ft
+# Actual value: $450,000
+
+value = estimate_home_value(3800, 5)
+
+print("Estimated valued:")
+print(value)
+
+#Notes
+'''
+ The process of modeling the value of something with 
+a set of fixed weights is called linear regression. 
+It's one of the simplest machine learning algorithms
+but with machine learning the computer comes up with
+the weights by itself by looking up the training data.
+'''
